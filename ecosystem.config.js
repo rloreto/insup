3ed64a35c91f73cd4578b9de56c0043b5f1f47de
roberto <rloreto@gmail.com>
2018-02-12 20:07:52 +0100

    add pm2 file

---
+++ ecosystem.config.js
@@ -0,0 +1,8 @@
+{
+  "apps" : [{
+    "name"        : "worker",
+    "script"      : "node ./instagram.js",
+    "args"        : "-s 'quierobesarte' 'Por.][12pPor.][12p'"
+    "watch"       : true
+  }]
+}
